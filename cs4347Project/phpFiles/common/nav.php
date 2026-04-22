75fa2f64201f8ac8b29c82b822650a7666bcef96
Rebec R <rebecaruni7@gmail.com>
2026-04-21 21:33:33 -0500

    nav_changes

---
+++ cs4347Project/phpFiles/common/nav.php
@@ -0,0 +1,33 @@
+<?php
+if(session_status(0 === PHP_SESSION_NONE))
+    {
+        session_start();
+    }
+
+$is_admin = isset($_SESSION['is_admin']) && $_SESSION['is_admin'] === true;
+?>
+
+<header>
+        <div class="top-bar"> <!--div for top bar: logo and profile-->
+            <div class="top-bar-placeholder"> </div>
+            <a class="logo-text" href="#">Logo</a> <!--logo image-->
+            <div class="profile-container"> <!--profile dropdown-->
+                <button class="profile-button">
+                    <?php echo htmlspecialchars($_SESSION['user_name'] ?? 'Account'); ?>
+                    <i class="fa-solid fa-angle-down"></i>
+                </button>
+
+            </div>
+        </div>
+        <nav> <!--bottom bar: navigation bar-->
+            <?php if ($is_admin):  
+            <a class="nav-a" href="#">Home</a>
+            <a class="nav-a" href="#">My Books</a>
+            <a class="nav-a" href="#">Account</a>
+        </nav>
+
+    </header>
+
+
+
+
